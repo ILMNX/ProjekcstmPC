b4217dacaaf2df8761ca83ab98a2715cd38f86f5
gilbertsibuea <87100242+ILMNX@users.noreply.github.com>
2022-10-18 19:26:43 +0700

    Add files via upload

--- tes6.cpp
+++ tes6.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <cmath>
 #include <conio.h>
+#include <stdlib.h>
 using namespace std;
 /*
 - Input & Output
@@ -14,7 +15,11 @@ int main(){
 	
 	//tipe data & variabel
 	int qty[15];
-	int hasil = 0;
+	int hasil1 = 0;
+	int hasil2=0;
+	int hasil3 =0;
+	int hasil4 = 0;
+	int hasil5 = 0;
 	int casing, cpu,gpu, mb,ram, 
 	hargacsing,hargacpu,hargagpu,hargamb,hargaram, 
 	bayar;
@@ -43,10 +48,8 @@ int main(){
 	for(int i = 0; i<1;i++){
 		cin>>qty[i];
 	}
-	for(int i=0;i < 1; i++){ // output bilangan dari array dengan looping
-		hasil+=qty[i]; // rumus penjumlahan semua bilangan dalam array
-		// versi pendek:   hasil+=nilai[i]
-		// versi panjang: hasil = hasil + nilai[i]
+	for(int i=0;i < 1; i++){ 
+		hasil1+=qty[i]; 
 	
 	//percabangan
 	if (casing == 1){
@@ -70,11 +73,11 @@ int main(){
 	
 	
 	cout<<endl;
-	cout<< "Anda Akan Membeli Casing PC  "<<ncasing<<"\nSejumlah: "<<hasil<<" Buah"<<"\nHarga  Rp."<<hargacsing<<endl;
+	cout<< "Anda Akan Membeli Casing PC  "<<ncasing<<"\nSejumlah: "<<hasil1<<" Buah"<<"\nHarga  Rp."<<hargacsing<<endl;
 	cout<<"Klik ENTER untuk melanjutkan"<<endl<<endl;
 	
 	getch();
-
+system("cls");
 	
 	//JENIS CPU
 	cout<<"************************************************"<<endl;
@@ -89,34 +92,41 @@ int main(){
 	cout<< "Silahkan Pilih Jenis CPU Anda (1-4): ";
 	cin>>cpu;
 	
+	cout<<"Jumlah Pembelian: ";
 	
-	
+	for(int i = 0; i<1;i++){
+		cin>>qty[i];
+	}
+	for(int i=0;i < 1; i++){ 
+		 hasil2+=qty[i]; 
+		
 	if (cpu == 1){
-		hargacpu  = 11900000;
+		hargacpu  = qty[i]*11900000;
 		ncpu = "Ryzen 9 7950X  ";
 	}
 	else if( cpu == 2){
-		hargacpu = 7600000;
+		hargacpu = qty[i]*7600000;
 		ncpu = "Intel Core i7 12700K";
 		
 	}
 	else if (cpu == 3){
-		hargacpu = 3999000;
+		hargacpu = qty[i]*3999000;
 		ncpu = "AMD RYZEN 7 5700X ";
 	}
 	else if (cpu == 4){
-		hargacpu = 2449000;
+		hargacpu = qty[i]*2449000;
 		ncpu = "INTEL CORE I5 11400";
 	}
 	else {
 		cout<< "Error! Inputan tidak ada (Silahkan Input angka 1-4)";
 		exit(0);
 	}
-	
-	cout<< "Anda akan Membeli CPU Jenis  "<<ncpu<<" dengan harga  Rp."<<hargacpu<<endl;
+}
+cout<<endl;
+		cout<< "Anda Akan Membeli CPU "<<ncpu<<"\nSejumlah: "<<hasil2<<" Buah"<<"\nHarga  Rp."<<hargacpu<<endl;
 	cout<<"Klik ENTER untuk melanjutkan"<<endl<<endl;
 	getch();
-	
+system("cls");
 	
 	//JENIS GPU
 	cout<<"************************************************"<<endl;
@@ -130,33 +140,41 @@ int main(){
 	cout<<endl;
 	cout<< "Silahkan Pilih Jenis GPU Anda (1-4): ";
 	cin>>gpu;
-		
 	
+	cout<<"Jumlah Pembelian: ";
+	for(int i = 0; i<1;i++){
+		cin>>qty[i];
+	}
+	for(int i=0;i < 1; i++){ 
+		hasil3+=qty[i]; 
 	if (gpu == 1){
-		hargagpu  = 3600000;
+		hargagpu  = qty[i]*3600000;
 		ngpu = "NVIDIA GTX 1660 Super ";
 	}
-	else if( casing == 2){
-		hargagpu = 1900000;
+	else if( gpu == 2){
+		hargagpu = qty[i]*1900000;
 		ngpu = "AMD RADEON RX 570 ";
 		
 	}
-	else if (casing == 3){
-		hargagpu = 4599000;
+	else if (gpu == 3){
+		hargagpu = qty[i]*4599000;
 		ngpu = "NVIDIA RTX 2060 ";
 	}
-	else if (casing == 4){
-		hargagpu = 11500000;
+	else if (gpu == 4){
+		hargagpu = qty[i]*11500000;
 		ngpu = "AMD RADEON 6600 XT ";
 	}
 	else {
 		cout<< "Error! Inputan tidak ada (Silahkan Input angka 1-4)";
 		exit(0);
 	}
-	
-	cout<< "Anda akan Membeli GPU Jenis  "<<ngpu<<" dengan harga  Rp."<<hargagpu<<endl;
+}
+	cout<<endl;
+	cout<< "Anda Akan Membeli GPU "<<ngpu<<"\nSejumlah: "<<hasil3<<" Buah"<<"\nHarga  Rp."<<hargagpu<<endl;
 	cout<<"Klik ENTER untuk melanjutkan"<<endl<<endl;
+	
 	getch();
+system("cls");	
 	
 	//Jenis Motherboard
 	cout<<"************************************************"<<endl;
@@ -170,33 +188,42 @@ int main(){
 	cout<<endl;
 	cout<< "Silahkan Pilih Jenis Motherboard Anda (1-4)  : ";
 	cin>>mb;
-		
 	
+	cout<<"Jumlah Pembelian: ";	
+	for(int i = 0; i<1;i++){
+		cin>>qty[i];
+	}
+	for(int i=0;i < 1; i++){ 
+		hasil4+=qty[i]; 
+		
 	if (mb == 1){
-		hargamb  = 3100000;
+		hargamb  = qty[i]*3100000;
 		nmboard = "ASUS TUF GAMING B660 ";
 	}
 	else if( mb == 2){
-		hargamb = 2650000;
+		hargamb = qty[i]*2650000;
 		nmboard = "ASUS TUF B560M-PLUS ";
 		
 	}
 	else if (mb == 3){
-		hargamb = 2199000;
+		hargamb = qty[i]*2199000;
 		nmboard = "ASUS ROG STRIX B450-F ";
 	}
 	else if (mb == 4){
-		hargamb = 5890000;
+		hargamb = qty[i]*5890000;
 		nmboard = "ASUS ROG STRIX Z690-A ";
 	}
 	else {
 		cout<< "Error! Inputan tidak ada (Silahkan Input angka 1-4)";
 		exit(0);
 	}
-	
-	cout<< "Anda akan Membeli Motherboard Jenis  "<<nmboard<<" dengan harga  Rp."<<hargamb<<endl;
+}
+	cout<<endl;
+	cout<< "Anda Akan Membeli Motherboard "<<nmboard<<"\nSejumlah: "<<hasil4<<" Buah"<<"\nHarga  Rp."<<hargamb<<endl;
 	cout<<"Klik ENTER untuk melanjutkan"<<endl<<endl;
+	
 	getch();
+system("cls");
 	//Jenis RAM
 	cout<<"******************************************"<<endl;
 	cout<<"|No|   |Jenis RAM      |    |    Harga    |"<<endl;
@@ -210,18 +237,23 @@ int main(){
 	cout <<"Silahkan Pilih Jenis RAM (1-3) : ";
 	cin>>ram;
 	
-	
+	cout<<"Jumlah Pembelian: ";
+	for(int i = 0; i<1;i++){
+		cin>>qty[i];
+	}
+	for(int i=0;i < 1; i++){ 
+		hasil5+=qty[i]; 
 	if (ram == 1){
-		hargaram  = 400000;
+		hargaram  = qty[i]*400000;
 		nram = "RAM 4GB ";
 	}
-	else if( casing == 2){
-		hargaram = 520000;
+	else if( ram == 2){
+		hargaram = qty[i]*520000;
 		nram = "RAM 8GB ";
 		
 	}
-	else if (casing == 3){
-		hargaram = 1300000;
+	else if (ram == 3){
+		hargaram = qty[i]*1300000;
 		nram = "RAM 16GB ";
 	}
 	else {
@@ -229,24 +261,42 @@ int main(){
 		exit(0);
 	}
 
-	
+
 	cout<<endl;
-	cout<< "Anda Akan Membeli RAM "<<nram<<" dengan Harga  Rp."<<hargaram<<endl;
+	cout<< "Anda Akan Membeli RAM "<<nram<<"\nSejumlah: "<<hasil5<<" Buah"<<"\nHarga  Rp."<<hargaram<<endl;
 	cout<<"Klik ENTER untuk melanjutkan"<<endl<<endl;
 	getch();
+system("cls");	
 	
 	
-	
-	/*
-	bayar = hargacsing * lama;
+	bayar = hargacsing+hargacpu+hargagpu+hargamb+hargaram;
 	cout<<"--------------------------------------"<<endl;
 	cout<<"Jenis casing anda adalah: "<<ncasing<<endl;
-	cout<<"Lama sewa anda adalah "<<lama<<" Malam"<<endl;
-	cout<<"Total harga penyewaan yang harus anda bayar adalah Rp."<<bayar<<endl;
+	cout<<"QTY : "<<hasil1<<endl;
+	cout<<"Total : "<<hargacsing<<endl<<endl;
+	
+	cout<<"Jenis CPU anda adalah: "<<ncpu<<endl;
+	cout<<"QTY : "<<hasil2<<endl;
+	cout<<"Total : "<<hargacpu<<endl<<endl;
+	
+	cout<<"Jenis GPU anda adalah: "<<ngpu<<endl;
+	cout<<"QTY : "<<hasil3<<endl;
+	cout<<"Total : "<<hargagpu<<endl<<endl;
+	
+	cout<<"Jenis MOTHERBOARD anda adalah: "<<nmboard<<endl;
+	cout<<"QTY : "<<hasil4<<endl;
+	cout<<"Total : "<<hargamb<<endl<<endl;
+	
+	cout<<"Jenis RAM anda adalah: "<<nram<<endl;
+	cout<<"QTY : "<<hasil5<<endl;
+	cout<<"Total : "<<hargaram<<endl<<endl;
+	cout<<"--------------------------------------"<<endl;
+	cout<<"Total Barang Anda Adalah: "<<hasil1+hasil2+hasil3+hasil4+hasil5<<endl;
+	cout<<"Total harga yang harus anda bayar adalah: Rp."<<bayar<<endl;
 	cout<<"--------------------------------------"<<endl;
 	getch();
-	*/
-	
+
+}
 	return 0;
 	
 }
